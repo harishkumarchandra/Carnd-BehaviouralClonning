9eef8df5c037acea6a9c5310b92d435363894dbc
harishkumarchandra <harishkumarchandra@gmail.com>
2017-10-21 10:47:48 -0400

    Comments

--- model.py
+++ model.py
@@ -3,6 +3,10 @@ import csv
 import numpy as np
 
 def getLinesFromDrivingLogs(dataPath, skipHeader=False):
+    """
+    Returns the lines from a driving log with base directory `dataPath`.
+    If the file include headers, pass `skipHeader=True`.
+    """
     lines = []
     with open(dataPath + '/driving_log.csv') as csvFile:
         reader = csv.reader(csvFile)
@@ -13,6 +17,15 @@ def getLinesFromDrivingLogs(dataPath, skipHeader=False):
     return lines
 
 def loadImageAndMeasurement(dataPath, imagePath, measurement, images, measurements):
+    """
+    Executes the following steps:
+      - Loads the image from `dataPath` and `imagPath`.
+      - Converts the image from BGR to RGB.
+      - Adds the image and `measurement` to `images` and `measurements`.
+      - Flips the image vertically.
+      - Inverts the sign of the `measurement`.
+      - Adds the flipped image and inverted `measurement` to `images` and `measurements`.
+    """
     originalImage = cv2.imread(dataPath + '/' + imagePath.strip())
     image = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
     images.append(image)
@@ -21,6 +34,12 @@ def loadImageAndMeasurement(dataPath, imagePath, measurement, images, measuremen
     measurements.append(measurement*-1.0)
 
 def loadImagesAndMeasurements(dataPath, skipHeader=False, correction=0.2):
+    """
+    Loads the images and measurements from the driving logs in the directory `dataPath`.
+    If the file include headers, pass `skipHeader=True`.
+    `correction` is the value to add/substract to the measurement to use side cameras.
+    Returns a pair `(images, measurements)`
+    """
     lines = getLinesFromDrivingLogs(dataPath, skipHeader)
     images = []
     measurements = []
@@ -40,12 +59,19 @@ from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
 def trainAndSave(model, inputs, outputs, modelFile, epochs = 3):
+    """
+    Train the model `model` using 'mse' lost and 'adam' optimizer for the epochs `epochs`.
+    The model is saved at `modelFile`
+    """
     model.compile(loss='mse', optimizer='adam')
     model.fit(inputs, outputs, validation_split=0.2, shuffle=True, nb_epoch=epochs)
     model.save(modelFile)
     print("Model saved at " + modelFile)
 
 def createPreProcessingLayers():
+    """
+    Creates a model with the initial pre-processing layers.
+    """
     model = Sequential()
     model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
     model.add(Cropping2D(cropping=((50,20), (0,0))))
@@ -53,6 +79,9 @@ def createPreProcessingLayers():
 
 
 def nVidiaModel():
+    """
+    Creates nVidea Autonomous Car Group model
+    """
     model = createPreProcessingLayers()
     model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
     model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
